ecfe11dd7b197f3b5b41516e9926865f4fac545b
Ibrahim Mudassar <22484328+ibrahimmudassar@users.noreply.github.com>
2025-02-04 04:26:02 -0500

    adding all 4 operations

--- speechtotext/src/components/global.tsx
+++ speechtotext/src/components/global.tsx
@@ -5,9 +5,23 @@ export const ArrayContext = createContext();
 
 export const ArrayProvider = ({ children }) => {
   const [splits, setSplits] = useState([]);
+  const [settings, setSettings] = useState({
+    isAdditionUsed: true,
+    isSubtractionUsed: true,
+    isMultiplicationUsed: true,
+    isDivisionUsed: true,
+    additionx1: 2,
+    additiony1: 100,
+    additionx2: 2,
+    additiony2: 100,
+    multiplicationx1: 2,
+    multiplicationy1: 12,
+    multiplicationx2: 2,
+    multiplicationy2: 100,
+  });
 
   return (
-    <ArrayContext.Provider value={{ splits, setSplits }}>
+    <ArrayContext.Provider value={{ splits, setSplits, settings, setSettings }}>
       {children}
     </ArrayContext.Provider>
   );
--- speechtotext/src/components/speech.tsx
+++ speechtotext/src/components/speech.tsx
@@ -8,9 +8,20 @@ import { Button, ButtonGroup } from "@heroui/button";
 import { useTimer } from "react-use-precision-timer";
 import { ArrayContext } from "@/components/global";
 import { useLocation } from "react-router-dom";
+import { IoSettingsSharp } from "react-icons/io5";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@heroui/modal";
+import { CheckboxGroup, Checkbox } from "@heroui/checkbox";
+import { Input } from "@heroui/input";
 
 const SpeechToText = () => {
-  const { splits, setSplits } = useContext(ArrayContext);
+  const { splits, setSplits, settings, setSettings } = useContext(ArrayContext);
 
   const location = useLocation();
 
@@ -54,13 +65,72 @@ const SpeechToText = () => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  const question = () => {
-    const oneRandomNum = generateRandomNumber(2, 12);
-    const twoRandomNum = generateRandomNumber(2, 100);
-    const product = oneRandomNum * twoRandomNum;
-    const question = `${Math.min(oneRandomNum, twoRandomNum)} * ${Math.max(oneRandomNum, twoRandomNum)}`;
+  const subtractionQuestion = () => {
+    const oneRandomNum = generateRandomNumber(
+      settings.additionx1,
+      settings.additiony1
+    );
+    const twoRandomNum = generateRandomNumber(
+      settings.additionx2,
+      settings.additiony2
+    );
+    const sum = oneRandomNum + twoRandomNum;
+    const question = `${sum} - ${oneRandomNum}`;
 
-    if (oneRandomNum * twoRandomNum < 10) {
+    if (sum < 10) {
+      setRandomNum({
+        question: question,
+        answer: [twoRandomNum, Small[twoRandomNum]],
+      });
+    } else {
+      setRandomNum({
+        question: question,
+        answer: [
+          twoRandomNum,
+          twoRandomNum.toString().replace(/(.{2})$/, ":$1"),
+        ],
+      });
+    }
+  };
+
+  const additionQuestion = () => {
+    const oneRandomNum = generateRandomNumber(
+      settings.additionx1,
+      settings.additiony1
+    );
+    const twoRandomNum = generateRandomNumber(
+      settings.additionx2,
+      settings.additiony2
+    );
+    const sum = oneRandomNum + twoRandomNum;
+    const question = `${oneRandomNum} + ${twoRandomNum}`;
+
+    if (sum < 10) {
+      setRandomNum({
+        question: question,
+        answer: [sum, Small[sum]],
+      });
+    } else {
+      setRandomNum({
+        question: question,
+        answer: [sum, sum.toString().replace(/(.{2})$/, ":$1")],
+      });
+    }
+  };
+
+  const multiplicationQuestion = () => {
+    const oneRandomNum = generateRandomNumber(
+      settings.multiplicationx1,
+      settings.multiplicationy1
+    );
+    const twoRandomNum = generateRandomNumber(
+      settings.multiplicationx2,
+      settings.multiplicationy2
+    );
+    const product = oneRandomNum * twoRandomNum;
+    const question = `${oneRandomNum} × ${twoRandomNum}`;
+
+    if (product < 10) {
       setRandomNum({
         question: question,
         answer: [product, Small[product]],
@@ -72,6 +142,53 @@ const SpeechToText = () => {
       });
     }
   };
+
+  const divisionQuestion = () => {
+    const oneRandomNum = generateRandomNumber(
+      settings.multiplicationx1,
+      settings.multiplicationy1
+    );
+    const twoRandomNum = generateRandomNumber(
+      settings.multiplicationx2,
+      settings.multiplicationy2
+    );
+    const product = oneRandomNum * twoRandomNum;
+    const question = `${product} ÷ ${oneRandomNum}`;
+
+    if (twoRandomNum < 10) {
+      setRandomNum({
+        question: question,
+        answer: [twoRandomNum, Small[twoRandomNum]],
+      });
+    } else {
+      setRandomNum({
+        question: question,
+        answer: [
+          twoRandomNum,
+          twoRandomNum.toString().replace(/(.{2})$/, ":$1"),
+        ],
+      });
+    }
+  };
+
+  const question = () => {
+    const operations = [];
+
+    if (settings.isAdditionUsed) operations.push("addition");
+    if (settings.isSubtractionUsed) operations.push("subtraction");
+    if (settings.isMultiplicationUsed) operations.push("multiplication");
+    if (settings.isDivisionUsed) operations.push("division");
+
+    if (operations.length === 0) return null;
+
+    const randomIndex = Math.floor(Math.random() * operations.length);
+
+    if (operations[randomIndex] == "addition") additionQuestion();
+    if (operations[randomIndex] == "subtraction") subtractionQuestion();
+    if (operations[randomIndex] == "multiplication") multiplicationQuestion();
+    if (operations[randomIndex] == "division") divisionQuestion();
+  };
+
   const reset = () => {
     resetTranscript();
     question();
@@ -91,17 +208,24 @@ const SpeechToText = () => {
     stopwatch.stop();
   };
 
+  // reverse the order of the object and ...splits if you want to reverse the order the data is shown
   useEffect(() => {
     if (randomNum.answer.some((v) => transcript.includes(v))) {
       setSplits([
-        ...splits,
         { question: randomNum.question, elapsed: elapsed },
+        ...splits,
       ]);
       resetTranscript();
       question();
       stopwatch.start();
     }
   }, [transcript, randomNum]);
+
+  const {
+    isOpen: isSettingsOpen,
+    onOpen: onSettingsOpen,
+    onOpenChange: onSettingsOpenChange,
+  } = useDisclosure();
 
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn&apos;t support speech recognition.</span>;
@@ -125,7 +249,187 @@ const SpeechToText = () => {
           <Button size="lg" onPress={reset} variant="shadow" color="warning">
             reset
           </Button>
+          <Button
+            isIconOnly
+            size="lg"
+            onPress={onSettingsOpen}
+            variant="shadow"
+          >
+            <IoSettingsSharp />
+          </Button>
         </ButtonGroup>
+        <Modal
+          isOpen={isSettingsOpen}
+          onOpenChange={onSettingsOpenChange}
+          size="2xl"
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Settings
+                </ModalHeader>
+                <ModalBody>
+                  <CheckboxGroup
+                    defaultValue={["add", "subtract", "multiply", "divide"]}
+                  >
+                    <Checkbox
+                      value="add"
+                      isSelected={settings.isAdditionUsed}
+                      onValueChange={(isSelected: boolean) =>
+                        setSettings({ ...settings, isAdditionUsed: isSelected })
+                      }
+                    >
+                      Addition
+                    </Checkbox>
+                    <div className="flex flex-row">
+                      <p>&emsp; &emsp;Range: (</p>
+                      <Input
+                        className="max-w-10"
+                        size="sm"
+                        value={settings.additionx1}
+                        onValueChange={(value: string) =>
+                          setSettings({
+                            ...settings,
+                            additionx1: parseInt(value),
+                          })
+                        }
+                      />
+                      <p>to</p>
+                      <Input
+                        className="max-w-10"
+                        size="sm"
+                        value={settings.additiony1}
+                        onValueChange={(value: string) =>
+                          setSettings({
+                            ...settings,
+                            additiony1: parseInt(value),
+                          })
+                        }
+                      />
+                      <p>) + (</p>
+                      <Input
+                        className="max-w-10"
+                        size="sm"
+                        value={settings.additionx2}
+                        onValueChange={(value: string) =>
+                          setSettings({
+                            ...settings,
+                            additionx2: parseInt(value),
+                          })
+                        }
+                      />
+                      <p>to</p>
+                      <Input
+                        className="max-w-10"
+                        size="sm"
+                        value={settings.additiony2}
+                        onValueChange={(value: string) =>
+                          setSettings({
+                            ...settings,
+                            additiony2: parseInt(value),
+                          })
+                        }
+                      />
+                      <p>)</p>
+                    </div>
+                    <Checkbox
+                      value="subtract"
+                      isSelected={settings.isSubtractionUsed}
+                      onValueChange={(isSelected: boolean) =>
+                        setSettings({
+                          ...settings,
+                          isSubtractionUsed: isSelected,
+                        })
+                      }
+                    >
+                      Subtraction
+                    </Checkbox>
+                    <p>&emsp; &emsp;Addition problems in reverse.</p>
+                    <Checkbox
+                      value="multiply"
+                      isSelected={settings.isMultiplicationUsed}
+                      onValueChange={(isSelected: boolean) =>
+                        setSettings({
+                          ...settings,
+                          isMultiplicationUsed: isSelected,
+                        })
+                      }
+                    >
+                      Multiplication
+                    </Checkbox>
+                    <div className="flex flex-row">
+                      <p>&emsp; &emsp;Range: (</p>
+                      <Input
+                        className="max-w-10"
+                        size="sm"
+                        value={settings.multiplicationx1}
+                        onValueChange={(value: string) =>
+                          setSettings({
+                            ...settings,
+                            multiplicationx1: parseInt(value),
+                          })
+                        }
+                      />
+                      <p>to</p>
+                      <Input
+                        className="max-w-10"
+                        size="sm"
+                        value={settings.multiplicationy1}
+                        onValueChange={(value: string) =>
+                          setSettings({
+                            ...settings,
+                            multiplicationy1: parseInt(value),
+                          })
+                        }
+                      />
+                      <p>) + (</p>
+                      <Input
+                        className="max-w-10"
+                        size="sm"
+                        value={settings.multiplicationx2}
+                        onValueChange={(value: string) =>
+                          setSettings({
+                            ...settings,
+                            multiplicationx2: parseInt(value),
+                          })
+                        }
+                      />
+                      <p>to</p>
+                      <Input
+                        className="max-w-10"
+                        size="sm"
+                        value={settings.multiplicationy2}
+                        onValueChange={(value: string) =>
+                          setSettings({
+                            ...settings,
+                            multiplicationy2: parseInt(value),
+                          })
+                        }
+                      />
+                      <p>)</p>
+                    </div>
+                    <Checkbox
+                      value="divide"
+                      isSelected={settings.isDivisionUsed}
+                      onValueChange={(isSelected: boolean) =>
+                        setSettings({ ...settings, isDivisionUsed: isSelected })
+                      }
+                    >
+                      Division
+                    </Checkbox>
+                    <p>&emsp; &emsp;Multiplication problems in reverse.</p>
+                  </CheckboxGroup>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="primary" onPress={onClose}>
+                    Save
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
       <div className="flex flex-col items-center gap-6">
         {elapsed > 0 ? (
