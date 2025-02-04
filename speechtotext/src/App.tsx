5929da5b53e4122362b63ea9c5615baa114c3bec
Ibrahim Mudassar <22484328+ibrahimmudassar@users.noreply.github.com>
2025-02-04 02:24:33 -0500

    adding analytics

--- speechtotext/src/App.tsx
+++ speechtotext/src/App.tsx
@@ -1,24 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import { ArrayProvider } from "@/components/global";
 
-
 import IndexPage from "@/pages/index";
 import DataPage from "@/pages/data";
 import PricingPage from "@/pages/pricing";
 import BlogPage from "@/pages/blog";
 import AboutPage from "@/pages/about";
 
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
   return (
-    <ArrayProvider>
-      <Routes>
-        <Route element={<IndexPage />} path="/" />
-        <Route element={<DataPage />} path="/data" />
-        <Route element={<PricingPage />} path="/pricing" />
-        <Route element={<BlogPage />} path="/blog" />
-        <Route element={<AboutPage />} path="/about" />
-      </Routes>
-    </ArrayProvider>
+    <>
+      <Analytics />
+      <ArrayProvider>
+        <Routes>
+          <Route element={<IndexPage />} path="/" />
+          <Route element={<DataPage />} path="/data" />
+          <Route element={<PricingPage />} path="/pricing" />
+          <Route element={<BlogPage />} path="/blog" />
+          <Route element={<AboutPage />} path="/about" />
+        </Routes>
+      </ArrayProvider>
+    </>
   );
 }
 
