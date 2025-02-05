cbe542a369a9058bdf0de84201acb520e2fa10be
Ibrahim Mudassar <22484328+ibrahimmudassar@users.noreply.github.com>
2025-02-05 01:28:21 -0500

    fixing navbar

--- speechtotext/src/components/navbar.tsx
+++ speechtotext/src/components/navbar.tsx
@@ -60,8 +60,8 @@ export const Navbar = () => {
         className="hidden sm:flex basis-1/5 sm:basis-full"
         justify="end"
       >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} title="Twitter">
+        <NavbarItem className="flex gap-2">
+          {/* <Link isExternal href={siteConfig.links.twitter} title="Twitter">
             <TwitterIcon className="text-default-500" />
           </Link>
           <Link isExternal href={siteConfig.links.discord} title="Discord">
@@ -69,8 +69,8 @@ export const Navbar = () => {
           </Link>
           <Link isExternal href={siteConfig.links.github} title="GitHub">
             <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
+          </Link> */}
+          {/* <ThemeSwitch /> */}
         </NavbarItem>
         {/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
         {/* <NavbarItem className="hidden md:flex">
@@ -87,10 +87,10 @@ export const Navbar = () => {
         </NavbarItem> */}
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal href={siteConfig.links.github}>
+      <NavbarContent className="basis-1 pl-4" justify="end">
+        {/* <Link isExternal href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
-        </Link>
+        </Link> */}
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
