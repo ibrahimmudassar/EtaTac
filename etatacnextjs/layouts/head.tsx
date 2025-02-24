2e6e0ff95c4bc79441c1f083389b8190ec273406
Ibrahim Mudassar <22484328+ibrahimmudassar@users.noreply.github.com>
2025-02-23 23:19:33 -0500

    adding meta tags

--- etatacnextjs/layouts/head.tsx
+++ etatacnextjs/layouts/head.tsx
@@ -6,6 +6,11 @@ import { siteConfig } from "@/config/site";
 export const Head = () => {
   return (
     <NextHead>
+      <meta
+        name="google-site-verification"
+        content="E318jQv9jD_Bv782_S7xZ6DlTPsXZ828IvgM0mqIMUQ"
+      />
+
       <title>{siteConfig.name}</title>
       <meta key="title" content={siteConfig.name} property="og:title" />
       <meta content={siteConfig.description} property="og:description" />
@@ -15,7 +20,15 @@ export const Head = () => {
         content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         name="viewport"
       />
-      <link href="/favicon.ico" rel="icon" />
+      <meta
+        property="og:image"
+        content="https://eta-tac.vercel.app/Pacioli.jpg"
+      />
+      <meta property="og:site_name" content="EtaTac" />
+      <meta property="og:url" content="https://eta-tac.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <link rel="icon" type="image/png" href="/mathoperations.png" />
+      <link rel="canonical" href="https://eta-tac.vercel.app/" />
     </NextHead>
   );
 };
